1e9a01964aa706cbca8e661082f533fb2d08913c
Kneyder <Kevinneyderbutronm@gmail.com>
2021-09-11 22:41:14 -0400

    commit

---
+++ src/bases/01-const-let.js
@@ -0,0 +1,14 @@
+
+const nombre = 'Kevin';
+const apellido = 'Neyder';
+let edad = 23;
+
+if(true){
+    let edad = 21;
+    console.log(edad);
+}
+
+
+console.log(nombre, apellido, edad);
+
+console.log('123456789ww');
